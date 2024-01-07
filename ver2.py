239fc65d98e820978408a214e52d3c166b839119
Dohuy5360 <loldh321@gmail.com>
2024-01-07 14:30:13 +0700

    Update: Add prompt to get class id by order number

--- ver2.py
+++ ver2.py
@@ -1,76 +1,140 @@
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 from colorama import Fore, Style
+from pprint import pprint
+from typing import List
 
-f = open("./draft/DanhSachLopHocPhan.txt", encoding="utf-8")
+class Course:
+    headers = ['STT', 'Loại', 'Mã LHP', 'Số lượng', 'Lịch học', 'Ghi chú', 'ID lớp lý thuyết']
+    def __init__(self, order: int = -1, type: str = "", code: str = "", remain: str = "", schedule: str = "", note: str = "", id: str = ""):
+        self.order = order
+        self.type = type
+        self.code = code
+        self.remain = remain
+        self.schedule = schedule
+        self.note = note
+        self.id = id
+
+    def getArrayData(self) -> List:
+        return [
+            self.order,
+            self.type,
+            self.code,
+            self.remain,
+            self.schedule,
+            self.note,
+            self.id,
+        ]
+    
+    def getCode(self):
+        return self.code
+
+class CourseManager:
+    courses : List[Course]= []
+
+    @staticmethod
+    def getCourse(order: int) -> Course | None:
+        for course in CourseManager.courses:
+            if(course.order == order):
+                return course
+        return None
+
+f = open("./draft/DanhSachLopCo1Loai.txt", encoding="utf-8")
 data = f.read()
 soup = BeautifulSoup(data, 'html.parser')
 table = soup.find('table')
 tbody = table.find('tbody')
 trCount = tbody.find_all(recursive=False)
 
-def viewTheoryClass(row, stt):
-    data = []
+
+def theoryClassDecorator(func):
+    def wrapper(*args, **kwargs):
+        print(Style.BRIGHT + Fore.GREEN + '\n Bảng lý thuyết:')
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+@theoryClassDecorator
+def viewTheoryClass(row, stt) -> Course:
     cells = row.find_all('td')
-    row_data = [cell.get_text(strip=True) for cell in cells if not cell.find('span') and cell.get_text(strip=True) != '']
-    del row_data[2]
-    row_data.insert(0,stt)
-    inputTag = row.find('input')
-    if (inputTag):
-        row_data.append(inputTag.get('id'))
-    else:
-        row_data.append('null')
-    data.append(row_data)
-    if (len(row_data) == 6):
-        headers = ['STT', 'Loại', 'Mã LHP', 'Số lượng', 'Lịch học', 'Ghi chú', 'ID lớp lý thuyết']
-    else:
-        headers = ['STT', 'Loại', 'Mã LHP', 'Số lượng', 'Lịch học', 'ID lớp lý thuyết']
-    print(Style.BRIGHT + Fore.GREEN + '\n Bảng lý thuyết:')
-    print(Fore.GREEN + Style.NORMAL + (tabulate(data, headers=headers, tablefmt='fancy_grid')))
-    return row_data[2]
+    row_data = [cell.get_text(strip=True) for cell in cells if not cell.find('span')]
+    # pprint(row_data)
+    courseId = row.find('input').get('id')
+    course = Course(stt, type=row_data[1],code=row_data[2], remain=row_data[4], schedule=row_data[5], note=row_data[7], id=courseId)
+    
+    print(Fore.GREEN + Style.NORMAL + (tabulate([course.getArrayData()], headers=course.headers, tablefmt='fancy_grid')))
+    return course
+
+
+def practiceClassDecorator(func):
+    def wrapper(*args, **kwargs):
+        print(Fore.BLUE + Style.BRIGHT + '''│\n└── Bảng thực hành tương ứng với:''', args[2].getCode())
+        result = func(*args, **kwargs)
+        print("-/"*90)
+        return result
+    return wrapper
+
+@practiceClassDecorator
+def viewPracticeClass(stt, row, theoryCourse: Course) -> List[Course]:
+    courses : List[Course] = []
+    rowsOfClass = row.find_all('tr')
+    for t in rowsOfClass[1:]: # exclude header
+        tds = t.find_all('td')
+        course = Course(
+            order=stt,
+            type="Thực hành",
+            code=tds[1].getText(strip=True),
+            remain=tds[2].getText(strip=True),
+            schedule=tds[3].getText(strip=True),
+            note=tds[4].getText(strip=True),
+            id=tds[0].find('input').get('id')
+        )
+        stt+=1
+        courses.append(course)
+        print(Fore.BLUE + Style.NORMAL + (tabulate([course.getArrayData()], headers=course.headers, tablefmt='mixed_grid')))
+    return courses
 
 def viewTheoryAndPracticeClass():
+    excludeName = "thực hành"
     stt = 1
     for i in range(0, len(trCount)):
         row = trCount[i]
         if (i % 2 == 0):
-            lhp = viewTheoryClass(row, stt)
-            stt+=1
+            theoryCourse = viewTheoryClass(row, stt)
+            CourseManager.courses.append(theoryCourse)
         else:
             span = row.find('span')
             if(span!=None):
-                if("thực hành" in span.get_text()):
-                    headers2 = []
-                    cells = row.find_all('tr')
-                    data = []
-                    for t in cells[1:]:
-                        tds = t.find_all('td')
-                        row_data_practice = []
-                        for tdi in range(0, len(tds)):
-                            tde = tds[tdi]
-                            if (tdi == 0):
-                                inp = tde.find('input')
-                                if inp:
-                                    id_attr = inp.get('id')
-                                    row_data_practice.append(id_attr)
-                            else:
-                                row_data_practice.insert(tdi-1, tde.getText(strip=True))
-                        row_data_no_empty = list(filter(None, row_data_practice))
-                        row_data_no_empty.insert(0,stt)
-                        stt+=1
-                        data.append(row_data_no_empty)
-                        if (len(row_data_no_empty) == 5):
-                            headers2 = ['STT', "Mã LHP", "SL còn lại",
-                                        "Lịch học", "Ghi chú", 'ID lớp thực hành']
-                        else:
-                            headers2 = ['STT', "Mã LHP", "SL còn lại",
-                                        "Lịch học", 'ID lớp thực hành']
-                    print(Fore.BLUE + Style.BRIGHT + '\n Bảng thực hành tương ứng với:', lhp)
-                    print(Fore.BLUE + Style.NORMAL + (tabulate(data, headers=headers2, tablefmt='mixed_grid')))
+                if(excludeName in span.get_text()):
+                    practiceCourses = viewPracticeClass(stt, row, theoryCourse)
+                    stt += len(practiceCourses) -1
+                    CourseManager.courses.extend(practiceCourses)
             else:
-                viewTheoryClass(row, stt)
-                stt+=1
+                theoryCourse = viewTheoryClass(row, stt)
+                CourseManager.courses.append(theoryCourse)
+        stt+=1
 
+def prompt(hint: str):
+    while True:
+        return input(hint)
+
+def askCourseCode():
+    while True:
+        classOrder = prompt("Please choose class by order number: ")
+        try:
+            courseFound = CourseManager.getCourse(int(classOrder))
+        except:
+            print("Please type correct course order!")
+            continue
+        if(courseFound is None):
+            print("Course not found.")
+            continue
+        else:
+            print("Your course code id: ",courseFound.code)
+            break
 
 if __name__ == "__main__":
     viewTheoryAndPracticeClass()
+    askCourseCode()
+    
+   
